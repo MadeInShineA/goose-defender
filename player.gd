5449fa9c5f3866d548ee06caba12d07e7ab1d34e
MadeInShineA <olivier.amacker@netplus.ch>
2025-03-15 14:38:20 +0100

    Cleaned the walking orientation + staff position depending on the mouse cursor position

--- player.gd
+++ player.gd
@@ -1,28 +1,31 @@
 extends CharacterBody2D
- 
- 
-const MAX_SPEED: int = 500
+
+const MAX_SPEED: int = 50
 const ACCELERATION: int = 10000
 const FRICTION: int = 100000
- 
-@onready var sprite: Sprite2D = $Sprite2D
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
- 
- 
+
+@onready var animation_player: AnimatedSprite2D = $AnimatedSprite2D
+@onready var staff: Node2D = $staff
+@onready var right_staff_position: Node2D = $right_staff_position
+@onready var left_staff_position: Node2D = $left_staff_position
+
 func _process(delta: float) -> void:
-	#if get_global_mouse_position().x < global_position.x:
-		#sprite.flip_h = true
-	#else:
-		#sprite.flip_h = false
- 
 	var direction = Input.get_vector("moving_left", "moving_right", "moving_up", "moving_down")
-	if direction:
+
+	if direction.length() > 0:
 		velocity.x = move_toward(velocity.x, MAX_SPEED * direction.x, ACCELERATION * delta)
 		velocity.y = move_toward(velocity.y, MAX_SPEED * direction.y, ACCELERATION * delta)
-		#animation_player.play("run")
 	else:
-		velocity.x = move_toward(velocity.x, MAX_SPEED * direction.x, FRICTION * delta)
-		velocity.y = move_toward(velocity.y, MAX_SPEED * direction.y, FRICTION * delta)
-		#animation_player.play("idle")
-	
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+		velocity.y = move_toward(velocity.y, 0, FRICTION * delta)
+
 	move_and_slide()
+
+	# Determine direction based on mouse position
+	var mouse_position = get_global_mouse_position()
+	if mouse_position.x < global_position.x:
+		animation_player.play("right")
+		staff.position = left_staff_position.position
+	else:
+		animation_player.play("left")
+		staff.position = right_staff_position.position
--- staff.gd
+++ staff.gd
@@ -4,13 +4,8 @@ const PROJECTILE = preload("res://projectile.tscn")
 @onready var fire_point: Marker2D = $Marker2D
 
 func _process(delta: float) -> void:
-	look_at(get_global_mouse_position())	
-	"""
-	if rotation_degrees > 90 and rotation_degrees < 270:
-		scale.y = -1
-	else:
-		scale.y = 1
-	"""
+	look_at(get_global_mouse_position())
+	
 	if Input.is_action_just_pressed("fire"):
 		var projectile_instance = PROJECTILE.instantiate()
 		get_tree().root.add_child(projectile_instance)
