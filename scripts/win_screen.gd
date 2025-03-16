335143cc5c8688659ceb31b8bd69af9d53fc094f
MadeInShineA <olivier.amacker@netplus.ch>
2025-03-16 15:59:12 +0100

    Added stafs and projectiles

--- scripts/win_screen.gd
+++ scripts/win_screen.gd
@@ -17,9 +17,6 @@ func _on_exit_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
-#TODO Add signal to show!
-
-
 func _on_won_game() -> void:
 	get_tree().paused = true
 	blur.show()
